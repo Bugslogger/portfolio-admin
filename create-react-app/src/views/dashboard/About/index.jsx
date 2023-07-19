e846038bbf7d565a2f13ae100ccd725f41af5b7a
Bugslogger <sarfarajshah009@gmail.com>
2023-07-19 19:02:29 +0530

    push

--- create-react-app/src/views/dashboard/About/index.jsx
+++ create-react-app/src/views/dashboard/About/index.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { Box, Button, Card, CardContent, Grid, TextField, Typography } from '@mui/material';
+import React from 'react';
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <Box>
+      <Grid container spacing={2}>
+        <Grid item md={6} xs={12}>
+          <Card sx={{ width: '100%' }}>
+            <CardContent>
+              <Typography variant="h2">About Me</Typography>
+              <Box sx={{ my: 2 }}></Box>
+              <TextField fullWidth label="About myself" multiline rows={12} color="secondary" />
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <Card sx={{ width: '100%' }}>
+            <CardContent>
+              <Typography variant="h2">Work</Typography>
+              <Box textAlign={'center'} sx={{ my: 2 }}>
+                <TextField fullWidth sx={{ my: 1 }} label="Company Name" color="secondary" />
+                <TextField fullWidth sx={{ my: 1 }} label="Role" color="secondary" />
+                <Button sx={{ my: 1 }} variant="contained" color="secondary">
+                  Submit
+                </Button>
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <Card sx={{ width: '100%' }}>
+            <CardContent>
+              <Typography variant="h2">About Me</Typography>
+              <Box sx={{ my: 2 }}></Box>
+              <TextField fullWidth label="About myself" multiline rows={12} color="secondary" />
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
 
-export default About
+export default About;
--- create-react-app/src/views/dashboard/Contact/index.jsx
+++ create-react-app/src/views/dashboard/Contact/index.jsx
@@ -1,7 +1,17 @@
+import { Box, Card, Typography } from '@mui/material';
 import React from 'react';
+import Table from './Table';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <Box>
+      <Typography variant='h2' my={2}>Client Message</Typography>
+      <Card elevation={8}>
+        <Table />
+      </Card>
+      
+    </Box>
+  );
 };
 
 export default Contact;
